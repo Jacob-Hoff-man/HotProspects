77dca40b428e44129459d721eb75918f94185640
Jacob Hoffman <cubhoffman@gmail.com>
2020-12-26 09:38:32 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Jacob Hoffman on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
