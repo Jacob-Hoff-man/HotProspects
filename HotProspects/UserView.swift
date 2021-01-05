21f01ab44278b8ccad0b1f788913f1441dc52f80
Jacob Hoffman <cubhoffman@gmail.com>
2021-01-05 16:22:54 -0500

    Version 1.0

---
+++ HotProspects/UserView.swift
@@ -0,0 +1,63 @@
+//
+//  UserView.swift
+//  HotProspects
+//
+//  Created by Jacob Hoffman on 12/27/20.
+//
+
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct UserView: View {
+    @State private var name = ""
+    @State private var emailAddress = ""
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Name: ", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email Address: ", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+            }
+            .navigationBarTitle("Your Code")
+        }
+        
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+    }
+}
